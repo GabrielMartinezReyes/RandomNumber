8118344855f1b7c05d0da0dd6183a8d47aaf3bd2
Gabriel Martinez <gabriel.martinez.reyes@gmail.com>
2017-11-16 09:38:03 +0100

    Create Random number

---
+++ RandomNumber/RandomNumber/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  RandomNumber
+//
+//  Created by Gabriel Martinez on 2017-11-15.
+//  Copyright © 2017 Gabriel Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
